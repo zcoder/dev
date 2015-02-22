1aea37ee8aeb60eea5789977afa642b2e3ae5219
Eugeniy <zhen.pub@gmail.com>
2015-02-22 03:53:32 +0300

    fix: typo

--- tools/skype_undel/actual.sh
+++ tools/skype_undel/actual.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ $1 <> "" ]]; then
+if [[ $1 != "" ]]; then
 sqlite3 -csv $1 'CREATE TRIGGER if not exists Messages_update_hook AFTER UPDATE OF body_xml    ON Messages begin update Messages set body_xml = case when new.body_xml <> "" then new.body_xml else old.body_xml end where id=new.id; end;'
 fi;
