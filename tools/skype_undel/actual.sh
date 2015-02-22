36cd49d105939de4ab02bca048d3999a0f1c001b
Eugeny Shablinsky <zhen.pub@gmail.com>
2015-02-22 15:22:23 +0300

    add: hint

--- tools/skype_undel/actual.sh
+++ tools/skype_undel/actual.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ "${1}" != "" ]]; then
-sqlite3 -csv "${1}" 'CREATE TRIGGER if not exists Messages_update_hook AFTER UPDATE OF body_xml    ON Messages begin update Messages set body_xml = case when new.body_xml <> "" then new.body_xml else old.body_xml end where id=new.id; end;'
+sqlite3 -csv "${1}" "drop trigger if exists Messages_update_hook; CREATE TRIGGER if not exists Messages_update_hook AFTER UPDATE OF body_xml    ON Messages begin update Messages set body_xml = case when new.body_xml <> '' then new.body_xml else old.body_xml || ' (deleted)' end where id=new.id; end;"
 fi;
